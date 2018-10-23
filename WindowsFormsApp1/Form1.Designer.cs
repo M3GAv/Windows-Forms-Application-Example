a43a49d89283f51d1cfc32e0f19d5d92206010a1
M3GAv <mateuszcelka@wp.pl>
2018-10-23 13:33:28 +0200

    added files

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("1.1");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("1.2");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("1.3");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("1", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("2.1");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("2.2");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("2", new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode6});
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstCity = new System.Windows.Forms.ListBox();
+            this.rdFemale = new System.Windows.Forms.RadioButton();
+            this.rdMale = new System.Windows.Forms.RadioButton();
+            this.chkC = new System.Windows.Forms.CheckBox();
+            this.chkASP = new System.Windows.Forms.CheckBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtAdress);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(205, 95);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Details";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(70, 26);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtAdress
+            // 
+            this.txtAdress.Location = new System.Drawing.Point(70, 56);
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(100, 20);
+            this.txtAdress.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adress";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // lstCity
+            // 
+            this.lstCity.FormattingEnabled = true;
+            this.lstCity.Items.AddRange(new object[] {
+            "Warsaw",
+            "Berlin",
+            "London"});
+            this.lstCity.Location = new System.Drawing.Point(25, 125);
+            this.lstCity.Name = "lstCity";
+            this.lstCity.Size = new System.Drawing.Size(120, 95);
+            this.lstCity.TabIndex = 1;
+            this.lstCity.SelectedIndexChanged += new System.EventHandler(this.lstCity_SelectedIndexChanged);
+            // 
+            // rdFemale
+            // 
+            this.rdFemale.AutoSize = true;
+            this.rdFemale.Location = new System.Drawing.Point(243, 67);
+            this.rdFemale.Name = "rdFemale";
+            this.rdFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdFemale.TabIndex = 2;
+            this.rdFemale.TabStop = true;
+            this.rdFemale.Text = "Female";
+            this.rdFemale.UseVisualStyleBackColor = true;
+            // 
+            // rdMale
+            // 
+            this.rdMale.AutoSize = true;
+            this.rdMale.Location = new System.Drawing.Point(243, 44);
+            this.rdMale.Name = "rdMale";
+            this.rdMale.Size = new System.Drawing.Size(48, 17);
+            this.rdMale.TabIndex = 3;
+            this.rdMale.TabStop = true;
+            this.rdMale.Text = "Male";
+            this.rdMale.UseVisualStyleBackColor = true;
+            // 
+            // chkC
+            // 
+            this.chkC.AutoSize = true;
+            this.chkC.Location = new System.Drawing.Point(243, 125);
+            this.chkC.Name = "chkC";
+            this.chkC.Size = new System.Drawing.Size(40, 17);
+            this.chkC.TabIndex = 4;
+            this.chkC.Text = "C#";
+            this.chkC.UseVisualStyleBackColor = true;
+            // 
+            // chkASP
+            // 
+            this.chkASP.AutoSize = true;
+            this.chkASP.Location = new System.Drawing.Point(243, 150);
+            this.chkASP.Name = "chkASP";
+            this.chkASP.Size = new System.Drawing.Size(72, 17);
+            this.chkASP.TabIndex = 5;
+            this.chkASP.Text = "ASP.NET";
+            this.chkASP.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(243, 197);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(370, 12);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Węzeł1";
+            treeNode1.Text = "1.1";
+            treeNode2.Name = "Węzeł2";
+            treeNode2.Text = "1.2";
+            treeNode3.Name = "Węzeł3";
+            treeNode3.Text = "1.3";
+            treeNode4.Name = "1";
+            treeNode4.Text = "1";
+            treeNode5.Name = "Węzeł5";
+            treeNode5.Text = "2.1";
+            treeNode6.Name = "Węzeł6";
+            treeNode6.Text = "2.2";
+            treeNode7.Name = "Węzeł4";
+            treeNode7.Text = "2";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode7});
+            this.treeView1.Size = new System.Drawing.Size(121, 130);
+            this.treeView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 246);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.chkASP);
+            this.Controls.Add(this.chkC);
+            this.Controls.Add(this.rdMale);
+            this.Controls.Add(this.rdFemale);
+            this.Controls.Add(this.lstCity);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAdress;
+        private System.Windows.Forms.ListBox lstCity;
+        private System.Windows.Forms.RadioButton rdFemale;
+        private System.Windows.Forms.RadioButton rdMale;
+        private System.Windows.Forms.CheckBox chkC;
+        private System.Windows.Forms.CheckBox chkASP;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
